5bd736da23f411a688a29b5c7c7d8b0bd68a87b8
Diego Molina <diemol@gmail.com>
2017-10-10 10:14:21 +0200

    Adding initial files

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Build
+#  docker build -t test_grid .
+FROM node:8
+
+RUN apt-get -qqy update \
+  && apt-get -qqy install \
+    git-core \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN git clone https://github.com/diemol/test_grid.git
+
+RUN npm install selenium-webdriver
+
+CMD ["node", "test.js"]
---
+++ test_win_env.js
@@ -0,0 +1,11 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+let driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .usingServer('http://docker.for.win.localhost:4444/wd/hub')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
